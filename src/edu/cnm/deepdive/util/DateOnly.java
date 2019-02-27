32f0754c0681c531110be35ff5ac76f542b6e74f
Ethan Lyons <ethan.lyons84@gmail.com>
2019-02-26 23:45:35 -0700

    Added months per year variable

--- src/edu/cnm/deepdive/util/DateOnly.java
+++ src/edu/cnm/deepdive/util/DateOnly.java
@@ -10,6 +10,7 @@ public class DateOnly {
   final int STANDARD_MONTHS_YEAR = 12;
   final int STANDARD = 1;
   final int GROUND_ZERO = 1970;
+  final int MONTHS_PER_YEAR = 12;
   private static final int[] DAYS_PER_MONTHS = {31,28,31,30,31,30,31,31,30,31,30,31};
   private static int yearDays;
   private static boolean isLeapYear(int year) {
@@ -30,14 +31,22 @@ public class DateOnly {
 
   public static int daysPerYear(int month)  {
 
-    for (int i = 0; i < 12; i++)  {
+    for (int i = 0; i < MONTHS_PER_YEAR; i++)  {
       yearDays += DAYS_PER_MONTHS[month];
       DAYS_PER_MONTHS[0] += 1;
-      System.out.println(yearDays);
-
     }
     return yearDays;
+  } // This will add up for each month in the
+  // year but how would I be able to stop at a certain number of months like 7 months?
+  //
+  // My initial thought is that there will be another loop that just adds on the remainder written as such
+
+  public static int remainderDays(int year, int month) {
+
+    if
+
   }
+
 
   public static int elapsedDays(int year, int month, int day) {
 
@@ -46,12 +55,10 @@ public class DateOnly {
 
     final int STANDARD_DAYS_YEAR = 365;
     final int STANDARD_MONTHS_YEAR = 12;
-    final int STANDARD = 1;
     final int GROUND_ZERO = 1970;
-    int yearDays = 0;
     int days = 0;
     int months = 0;
-    days = (year - GROUND_ZERO) * STANDARD_DAYS_YEAR;
+    days = (year - GROUND_ZERO) * yearDays;
     months = (year - GROUND_ZERO) * DAYS_PER_MONTHS[month];
     System.out.println(days);
     return days;
